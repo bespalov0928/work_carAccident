52094fe76eaa6139644d2cecc0d427061c5af34b
bespalov0928 <bespalov0928@mail.ru>
2023-03-05 10:57:11 +0700

    work_car_accident: Добавил модели

---
+++ src/main/java/ru/work/workcaraccident/model/User.java
@@ -0,0 +1,27 @@
+package ru.work.workcaraccident.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String password;
+    private String username;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+    private boolean enabled;
+}
